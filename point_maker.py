61055724ec5b1e84364b63fd8226bded4ce6cf4c
Hadi Youssef <hmyoussef@gmail.com>
2024-06-22 16:48:12 -0400

    Chain that turns script into a presentation

--- point_maker.py
+++ point_maker.py
@@ -1,7 +1,5 @@
-import os
-import json
+import os, json, ast, re
 from dotenv import load_dotenv
-
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import CharacterTextSplitter
@@ -14,21 +12,46 @@ from langchain import PromptTemplate
 from langchain.tools import BaseTool
 from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
 
-text = """Euclid's theorem is an important result in mathematics, 
-specifically in number theory. The theorem essentially states that there are an infinite 
-number of prime numbers. Prime numbers are numbers that only have two factors: one and itself.
-There are several proofs on this theorem, first being proven by Euclid himself in his book titled 
-"Elements." A rudimentary explanation of this proof is as follows: Assume that there is a finite set 
-consisting of all prime numbers. We can show that there is at least one prime number not included in this list. 
-Let P be the product of all prime numbers, i.e. P = p1p2p3...pk. Then, let Q be P + 1. Then we can show that
-q is prime or not. If q is prime, then q is not in the list, therefore ends the proof. If q is not prime, then 
-some factor p divides q, which then, since P is a product of all p, then p divides P. If p divides P and p divides q, 
-which is P + 1, then it must follow that p divides the difference of P and P + 1. However, no prime number 
-divides 1, p cannot be in this list, thus a contradiction."""
+text = """To live, or not to live: that is the question. Is it more noble to put up with all the difficulties that fate throws our way, or to fight against them, and, in fighting them, put an end to everything? Death is like sleeping, that's all. A kind of sleep that ends the countless heartaches and sufferings that are part of life – now that's something to be desired. To die, to sleep – and during this sleep, maybe we dream.
+
+But that's the problem. In that sleep-like death, when we've finally been released from this painful life, who knows what dreams we'll have? That's enough to make us hesitate, to keep on suffering, and to put up with a horrible existence for so long. Otherwise, who would put up with the slaps and insults we endure over time, the wrongs done to us by the powerful, the arrogance of proud men, the heartaches of rejected love, the bureaucracy in the courts, the rudeness of bureaucrats, and the offenses dished out by unworthy people, that we patiently accept– when we could just settle our account with God by using a dagger on ourselves?
+
+Who would carry these heavy burdens, grunting and sweating under an exhausting life, if it weren't for the fear of something worse in the afterlife, that unknown country from whose border nobody ever returns? This fear bewilders us, and makes us prefer the troubles we know, rather than run off to troubles we don't know. In the end, contemplating the afterlife makes us cowards, and the bright color of our bravery is turned pale by our contemplation. And all our ambitious plans get put on hold while we think about this, and end up never being executed."""
 
 load_dotenv()
 
 openai_api_key = os.getenv('OPENAI_API_KEY')
+
+system_points_prompt = """You are a point maker. You are given an input in the following form
+
+TITLE OF PRESENTATION
+|||||
+Title of Slide
+Info of slide 
+#####
+Title of Slide 2
+Info of slide 2
+#####
+...
+#####
+Title of Slide X
+Info of slide X
+#####
+
+You are to turn the INFO of each slide into a couple of bullet points fit for a google slide presentation. 
+You are to output in a $JSON_BLOB
+{{\n\
+    "action": "Make Points",
+    "point presentation": "Return the presentation in a list format like so: [Title of Presentation, [Title of Slide 1, Point 1 of Slide 1, Point 2 of Slide 1, Point 3 of Slide 1..], [Title of Slide 2, Point 1 of Slide 2, Point 2 of Slide 2,...],...,[Title of Slide X, Point 1 of Slide X, Point 2 of Slide X, ..., Point X of Slide X]]."
+}}\n\
+"""
+
+human_points_prompt="""Here is the presentation:
+
+{presentation}
+
+Please turn it into a point format list and return it
+"""
 
 system_organizer_prompt = """You are a slide organizer. You are given the script to a speech, and are to 
 reorganize the speech so that it is presentable, under different topics. You will start by providing a title for 
@@ -74,24 +97,69 @@ organization_prompt = ChatPromptTemplate.from_messages([
     HumanMessagePromptTemplate(prompt=PromptTemplate(input_variables=['script'], template=human_organizer_prompt)),
 ])
 
+point_maker_prompt = ChatPromptTemplate.from_messages([
+    SystemMessagePromptTemplate(prompt=PromptTemplate(input_variables=[], template=system_points_prompt)),
+    HumanMessagePromptTemplate(prompt=PromptTemplate(input_variables=['presentation'], template=human_points_prompt)),
+])
+
 llm = ChatOpenAI(temperature=0.3, model="gpt-3.5-turbo-0125")
 
 organizer = (organization_prompt | llm)
+point_maker = (point_maker_prompt | llm)
+
+def extract_outermost_list(pres_list):
+    start = -1
+    open_brackets = 0
+
+    for i, char in enumerate(pres_list):
+        if char == '[':
+            if open_brackets == 0:
+                start = i
+            open_brackets += 1
+        elif char == ']':
+            open_brackets -= 1
+            if open_brackets == 0:
+                end = i + 1
+                return pres_list[start:end]
+
+    return None
 
 response = organizer.invoke({"script": text}).content
+
 response = response[52:]
 response = response[:-3]
-print(response)
-# phrase = "\"presentation\": \""
-# print(phrase)
-# sliding_window = ""
-# counter = 0
-# for letter in response:
-#     if len(sliding_window) == 17:
-#         sliding_window = sliding_window[1:]
-#     sliding_window += letter
-#     if sliding_window == phrase:
-#         print(counter)
-#     counter += 1
+
+
+
+extracted_list = []
+pres_list = point_maker.invoke({"presentation": response}).content
+extracted_list = extract_outermost_list(pres_list)
+print("Extracted List", extracted_list)
+parsed_list = ast.literal_eval(extracted_list)
+
+# Printing out in terminal for visual pleasure
+final_list = []
+first = True
+first_mini = True
+idx = 0
+first = True
+first_mini = True
+for value in parsed_list:
+    if first:
+        print("\n")
+        print(value)
+        print("\n")
+        first = False
+    elif not first:
+        for point in value:
+            if first_mini:
+                print("\n")
+                print(point)
+                print("\n")
+                first_mini = False
+            else:
+                print(f"- {point}")
+    first_mini = True
+
 
 
